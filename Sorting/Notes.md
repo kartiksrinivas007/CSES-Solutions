4dd3692917a89c828f192b29dbac7b6192244d14
kartiksrinivas007 <kartiksrinivas008@gmail.com>
2022-07-07 14:26:48 +0530

    Two KNights SOlved

---
+++ Sorting/Notes.md
@@ -0,0 +1,4 @@
+# Permutations
+---
+
+Was a simple observation of breaking the permutations into two parts an odd series and an even series?
---
+++ Sorting/two_knights.cpp
@@ -0,0 +1,18 @@
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+
+int main()
+{
+
+    int n;
+    cin >> n;
+
+    for (int k = 1; k <= n; k++)
+    {
+        cout << (1ll * k*k * 1ll * (k*k - 1) / 2) - (4* 1ll * (k-1) * (k-2)) << endl;
+    }     
+}
